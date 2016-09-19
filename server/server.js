5c051efd6d9ac454752d0e35bd71894a3cf61eb3
Tom D <tdror53@gmail.com>
2016-09-19 15:27:25 -0400

    finally!, time to fix the sql calls

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ REST.prototype.configureExpress = function(connection) {
   	app.use(bodyParser.urlencoded({ extended: true }));
  	  app.use(bodyParser.json());
   	var router = express.Router();
-  	app.use('/api', router);
+  	app.use('/', router);
   	var rest_router = new rest(router,connection);
   	// var rest_router = new rest(router,connection,md5);
   	self.startServer();
