451ff000587af1e0d8c7f66b51fa30cbdbd497d1
Tom D <tdror53@gmail.com>
2016-09-20 11:03:10 -0400

    allow origin

--- server/server.js
+++ server/server.js
@@ -50,6 +50,22 @@ REST.prototype.configureExpress = function(connection) {
  	  app.use(bodyParser.json());
   	var router = express.Router();
   	app.use('/', router);
+
+    //
+    app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.setHeader('Access-Control-Allow-Origin', 'http://itsabouttime.herokuapp.com');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        // res.setHeader('Access-Control-Allow-Credentials', true);
+        // Pass to next layer of middleware
+        next();
+    });
   	var rest_router = new rest(router,connection);
   	// var rest_router = new rest(router,connection,md5);
   	self.startServer();
